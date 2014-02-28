f72b19ad07981da3f3b54f2b9efa8277dfdbbd23
Sebastian Springer <sebastian.springer@mayflower.de>
2014-02-28 14:28:45 +0100

    Templating

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var hbs = require('express-hbs');
 var routes = require('./routes');
 
 var app = express();
+
+app.engine('hbs', hbs.express3());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 routes(app);
 
