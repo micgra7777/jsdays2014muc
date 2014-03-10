275d5c48809f4256ca65f4d1e629d40ee1b7001e
Sebastian Springer <sebastian.springer@mayflower.de>
2014-03-10 08:56:54 +0100

    ...

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var hbs = require('express-hbs');
 var routes = require('./routes');
+var path = require('path');
 
 var app = express();
 
 app.engine('hbs', hbs.express3());
-app.set('views', require('path').join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.use(express.logger());
 
 app.use(express.static(__dirname+'/public'));
 
